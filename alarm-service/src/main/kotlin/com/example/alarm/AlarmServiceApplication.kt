c43acd4887d604178dadf27a4af02e8dfb836996
wldks1008 <gayeon1008@g.skku.edu>
2026-06-11 00:06:36 +0900

    feat: add cached feature flag client

--- alarm-service/src/main/kotlin/com/example/alarm/AlarmServiceApplication.kt
+++ alarm-service/src/main/kotlin/com/example/alarm/AlarmServiceApplication.kt
@@ -1,9 +1,12 @@
 package com.example.alarm
 
+import com.example.alarm.featureflag.FeatureFlagProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(FeatureFlagProperties::class)
 class AlarmServiceApplication
 
 fun main(args: Array<String>) {
